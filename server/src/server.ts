659e61eee95e43ef1dae5e4944884fe813a9902a
André Silva <andreoew@hotmail.com>
2025-02-18 12:34:39 -0300

    create setup backend end route subscription

---
+++ server/src/server.ts
@@ -0,0 +1,42 @@
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import {
+  validatorCompiler,
+  serializerCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { subcribeToEventRoute } from './routes/subscribe-to-event-route'
+import { env } from './env'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subcribeToEventRoute)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server running!')
+})
